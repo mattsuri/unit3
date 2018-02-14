9199b6174d72679a8687e7cf85c21f8fedd14061
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-02-14 08:20:53 -0500

    Updated from Brython Server: 2/14/2018 8:20:53 AM

--- loopDemo.py
+++ loopDemo.py
@@ -6,6 +6,27 @@
 
 
 
+"""
+#I love comp sci 5 times
+
+for i in range(1,6):
+    print("I <3 Computer Science")
+"""
+"""
+
+#Counts from 1 to 10
+
+for i in range(1,11):
+    print(i)
+    
+"""    
+"""
+#Count from 28 to 43 by 2s
+for i in range(27,44, 2):
+    print(i)
+
+"""
+
 
 #add up the numbers from 1 to 5
 
