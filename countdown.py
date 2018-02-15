e9b66b5937ccadbb7ce83abeb63831ecf69e6f2b
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-02-15 08:37:03 -0500

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,1 +1,16 @@
+#Matthew Suriawinata
+#2/15/18
+#coutdown.py - counts down
 
+num = int(input("Enter a number of seconds: "))
+
+
+
+while True:
+    num -= 1
+    if num == 0:
+        print("BOOM")
+        break
+    print(num)
+    
+    
