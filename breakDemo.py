8734b20b38330b11f17c569c548a67fff538cbae
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-02-15 08:10:57 -0500

    Updated from Brython Server: 2/15/2018 8:11:09 AM

--- breakDemo.py
+++ breakDemo.py
@@ -1,1 +1,12 @@
+#Matthew Suriawinata
+#2/15/18
+#breakDemo.py - how to break a loop 
 
+while True:
+    text = input("Say anything: ")
+    if text == "anything" :
+        break
+    print(text)
+    
+    
+    
