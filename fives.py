63e2307e82276304a94baba0b585672a97ecdc76
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-02-15 08:25:51 -0500

    Updated from Brython Server: 2/15/2018 8:25:48 AM

--- fives.py
+++ fives.py
@@ -1,11 +1,11 @@
 #Matthew Suriawinata
 #2/15/18
-#fives.py - counts by multiples of five
+#squares.py - prints squares
 
 num = int(input("Enter a number: "))
-numint = 0
+
 i = 0
 
 while i < num:
-    i += 5
-    print(i)
+    i += 1
+    print(i**2)
