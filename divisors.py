0b2b4133dc459b038dfad28ce65880ac5edb040a
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-02-26 08:45:26 -0500

    Updated from Brython Server: 2/26/2018 8:45:24 AM

--- divisors.py
+++ divisors.py
@@ -1,15 +1,12 @@
 #Matthew Suriawinata
 #2/26/18
-#divisors.py = print divisor of a number
+#divisors.py - print divisor of a number
 
-num = int(input("Enter a number ot divide: "))
+num = int(input("Enter a number to divide: "))
 multi = num
 
-while True:
-    if multi == 1:
-        break
-        
-    elif num % multi == 0:
+while multi != 1:
+    if num % multi == 0:
         print(num/multi)
-        multi -+ 1
+    multi -= 1
     
