648e3d00a5ddc297ab9bad82bd85a534cefb8096
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-01 08:48:08 -0500

    Updated from Brython Server: 3/1/2018 8:48:04 AM

--- fri13.py
+++ fri13.py
@@ -5,4 +5,25 @@
 
 from datetime import date
 
+yearNow = date.today().year
+monthNow = date.today().month
+dayNow = date.today().day
+
+monthAdd = 0
+yearAdd = 0
+
+
+if (dayNow > 13 and yearNow == 12):
+
+if dayNow > 13:
+    monthAdd = 1
+else:
+    monthAdd = 0
+    
+    
+while True:
+    if weekday(yearNow + yearAdd, monthNow +monthAdd, 13) == 4:
+        print(
+    
+
 
