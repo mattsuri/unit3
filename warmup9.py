204309823724750c1c76cf50ae203797814e6374
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-01 08:29:12 -0500

    Updated from Brython Server: 3/1/2018 8:29:08 AM

--- warmup9.py
+++ warmup9.py
@@ -1,15 +1,12 @@
 #Matthew Suriawianta
 #3/1/18
-#warmup9.py - Ask user to enter text 
+#perfectNumber.py - Find if number is perfect
 
+total = 0
+num = int(input("Enter a number: ")
+divisor = num - 1
 
-text = input("Enter some text: ")
-
-text = (text.lower())
-
-
-for ch in text:
-    if ch == "a" or ch == "e"or ch == "o" or ch == "i" or ch == "u":
-        print(ch.upper())
-    else:
-        print(ch.lower())
+while divisor>0:
+    if num % divisor == 0:
+        total += num
+    divsior -=1
