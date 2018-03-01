6e2346cba4eec7a48319605a949c882a8be42f07
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-01 08:21:44 -0500

    Updated from Brython Server: 3/1/2018 8:21:41 AM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,14 @@
+#Matthew Suriawianta
+#3/1/18
+#warmup9.py - Ask user to enter text 
 
+
+text = input("Enter some text: ")
+
+
+
+for ch in text:
+    if ch == "a", ch == "e", ch == "i", ch == "i", ch == "u":
+        print(ch.upper())
+    else:
+        print(ch.lower())
