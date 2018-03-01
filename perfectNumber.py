48e0d679706c7a1cebae389e8e0c65b12fb18a7c
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-01 08:29:37 -0500

    Update perfectNumber.py

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,12 @@
+#Matthew Suriawianta
+#3/1/18
+#perfectNumber.py - Find if number is perfect
 
+total = 0
+num = int(input("Enter a number: ")
+divisor = num - 1
+
+while divisor>0:
+    if num % divisor == 0:
+        total += num
+    divsior -=1
