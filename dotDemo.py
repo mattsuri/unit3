539d33834310bf017fced599d3e67c66aee13fb2
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-01 08:07:05 -0500

    Updated from Brython Server: 3/1/2018 8:07:01 AM

--- dotDemo.py
+++ dotDemo.py
@@ -1,1 +1,20 @@
+#Matthew Suriawinata
+#3/1/18
+#dotDemo.py - how to use loops with graphics
+
+from ggame import *
+
+red = Color("0xFF000", 1)
+
+
+dot = CircleAsset(25, LineStyle(1, red), red)
+
+
+Sprite(dot)
+
+App().run()
+
+
+
+
 
