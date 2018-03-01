126da139bbf39d99118b04d3285c4ab9e02e9639
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-01 08:23:44 -0500

    Updated from Brython Server: 3/1/2018 8:23:40 AM

--- warmup9.py
+++ warmup9.py
@@ -5,10 +5,11 @@
 
 text = input("Enter some text: ")
 
+text = (text.lower())
 
 
 for ch in text:
-    if ch == "a", ch == "e", ch == "i", ch == "i", ch == "u":
+    if ch == "a" or ch == "e"or ch == "o" or ch == "i" or ch == "u":
         print(ch.upper())
     else:
         print(ch.lower())
